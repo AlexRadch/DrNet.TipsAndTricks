e36afa9d69c43aad99cd1d4d62bf0b9404ff2893
Alexander Radchenko <radchenkosasha@gmail.com>
2022-11-22 05:09:33 +0600

    Raw String Literals Comments And Line Continuation

---
+++ src/RawStringLiterals/CommentsAndLineContinuation/Program.cs
@@ -0,0 +1,83 @@
+﻿{
+    var literal = $"""
+        Line 1
+        Сomment 1
+        Comment 2
+        Line 2 Comment 3
+        Line 3
+        continuation of line 3
+        Empty Line Comment
+
+        """;
+    Console.WriteLine(literal);
+}
+{
+    var literal = $"""
+        Line 1
+        //Сomment 1
+        //Comment 2
+        Line 2/* Comment 3*/
+        Line 3
+        continuation of line 3
+        //Empty Line Comment
+
+        """;
+    Console.WriteLine(literal);
+}
+{
+    var literal = $"""
+        Line 1
+        {""
+
+        //Сomment 1
+        //Comment 2
+       }Line 2{""   /* Comment 3*/  }
+        Line 3
+        continuation of line 3
+        {""
+        
+        //Empty Line Comment
+       }
+        """;
+    Console.WriteLine(literal);
+}
+{
+    var literal = $"""
+        Line 1
+        {""
+
+        //Сomment 1
+        //Comment 2
+       }Line 2{""   /* Comment 3*/  }
+        Line 3 {""
+       }continuation of line 3
+        {""
+        
+        //Empty Line Comment
+       }
+        """;
+    Console.WriteLine(literal);
+}
+{
+    var literal = $$$"""
+        Line 1
+        {{{""
+
+       //Сomment 1
+       //Comment 2
+     }}}Line 2{{{""   /* Comment 3*/  }}}
+        Line 3 {{{""
+     }}}continuation of line 3
+        {{{""
+
+       //Empty Line Comment
+     }}}
+        """;
+    Console.WriteLine(literal);
+}
+{
+    var literal = $$$"""
+        
+        """;
+    Console.Write(literal);
+}
