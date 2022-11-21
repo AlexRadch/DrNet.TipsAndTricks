365f9936aaadd3520d105750a1b48b2ac83a3781
Alexander Radchenko <radchenkosasha@gmail.com>
2022-11-22 00:40:31 +0600

    WhatsNew C# 11

---
+++ src/WhatsNew/CS11/GenericAttributes/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Before C# 11:
+public class TypeAttribute : Attribute
+{
+    public TypeAttribute(Type t) => MyType = t;
+
+    public Type MyType { get; }
+}
+
+// C# 11 feature:
+public class GenericAttribute<T> : Attribute
+    where T : class
+{
+}
+
+
+public class GenericClass<T>
+    where T : class
+{
+    [Type(typeof(string))]
+    [Generic<string>] 
+    //[Generic<T>] // Not allowed! Generic attributes must be fully constructed types.
+    public string Method() => string.Empty;
+}
---
+++ src/WhatsNew/CS11/GenericMath/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Numerics;
+
+var ints = Generate(new Random(), random => random.Next(100)).Take(5).ToArray();
+
+var sum1 = Sum1(ints); // Compiler error. See https://github.com/dotnet/roslyn/issues/65534
+var sum2 = Sum<int, double>(ints);
+var avr = Average<int, double>(ints);
+var dev = StandardDeviation<int, double>(ints);
+
+Console.WriteLine($"""
+    Sum1 = {sum1}
+    Sum2 = {sum2}
+    Avr = {avr}
+    dev = {dev}
+
+    """);
+
+var longs = Generate(new Random(), random => random.NextInt64(100)).Take(5).ToArray();
+
+//sum1 = Sum1(longs);
+sum2 = Sum<long, double>(longs);
+avr = Average<long, double>(longs);
+dev = StandardDeviation<long, double>(longs);
+
+Console.WriteLine($"""
+    {""//Sum1 = {sum1}
+   }Sum2 = {sum2}
+    Avr = {avr}
+    dev = {dev}{""
+   } // the same line
+
+    """);
+
+static double Sum1(IEnumerable<int> values) // Compiler error. See https://github.com/dotnet/roslyn/issues/65534
+{
+    long result = 0;
+
+    foreach (var value in values)
+    {
+        result += value;
+    }
+
+    return result;
+}
+
+static TResult Sum<T, TResult>(IEnumerable<T> values)
+    where T : INumberBase<T>
+    where TResult : INumberBase<TResult>
+{
+    TResult result = TResult.Zero;
+
+    foreach (var value in values)
+    {
+        checked { result += TResult.CreateChecked(value); }
+    }
+
+    return result;
+}
+
+static TResult Average<T, TResult>(IEnumerable<T> values)
+    where T : INumberBase<T>
+    where TResult : INumberBase<TResult>
+{
+    TResult sum = Sum<T, TResult>(values);
+    return sum / TResult.CreateChecked(values.Count());
+}
+
+static TResult StandardDeviation<T, TResult>(IEnumerable<T> values)
+    where T : INumberBase<T>
+    where TResult : IRootFunctions<TResult>
+{
+    TResult standardDeviation = TResult.Zero;
+
+    if (values.Any())
+    {
+        TResult average = Average<T, TResult>(values);
+        TResult sum = Sum<TResult, TResult>(values.Select((value) => {
+            var deviation = TResult.CreateChecked(value) - average;
+            return deviation * deviation;
+        }));
+        standardDeviation = TResult.Sqrt(sum / TResult.CreateSaturating(values.Count() - 1));
+    }
+
+    return standardDeviation;
+}
+
+static IEnumerable<TResult> Generate<TGenerator, TResult>(TGenerator generator, Func<TGenerator, TResult> next)
+{
+    while (true)
+    {
+        yield return next(generator);
+    }
+}
