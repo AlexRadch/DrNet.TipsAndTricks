b7378afaafc8536fc1a9940c44b48fcefffd3709
Alexander Radchenko <radchenkosasha@gmail.com>
2022-11-23 00:09:55 +0600

    CommentsRawStringLiterals

--- src/RawStringLiterals/CommentsInRawStringLiterals/Program.cs
+++ src/RawStringLiterals/CommentsInRawStringLiterals/Program.cs
@@ -12,6 +12,7 @@
     Console.WriteLine(literal);
 }
 {
+    // Use `Ctrl+K, Ctrl+C` to comment a some text in IDE
     var literal = $"""
         Line 1
         //Сomment 1
@@ -25,6 +26,7 @@
     Console.WriteLine(literal);
 }
 {
+    // Use interpolation with empty string `{"" /* comment is here*/ }` around comments to remove them from result string
     var literal = $"""
         Line 1
         {""
@@ -42,6 +44,7 @@
     Console.WriteLine(literal);
 }
 {
+    // Use interpolation with empty string `{"" }` between lines to concatinate next line to privious without line break
     var literal = $"""
         Line 1
         {""
@@ -49,7 +52,7 @@
         //Сomment 1
         //Comment 2
        }Line 2{""   /* Comment 3*/  }
-        Line 3 {""
+        Line 3 {"" // interpolation with empty string to concatinate next line without line break
        }continuation of line 3
         {""
         
@@ -59,6 +62,7 @@
     Console.WriteLine(literal);
 }
 {
+    // When interpolation use many curly brackets `{{{` then comments looks ugly 
     var literal = $$$"""
         Line 1
         {{{""
@@ -66,7 +70,7 @@
        //Сomment 1
        //Comment 2
      }}}Line 2{{{""   /* Comment 3*/  }}}
-        Line 3 {{{""
+        Line 3 {{{"" // interpolation with empty string to concatinate next line without line break
      }}}continuation of line 3
         {{{""
 
@@ -75,9 +79,5 @@
         """;
     Console.WriteLine(literal);
 }
-{
-    var literal = $$$"""
-        
-        """;
-    Console.Write(literal);
-}
+
+Console.WriteLine("Bye bye");
