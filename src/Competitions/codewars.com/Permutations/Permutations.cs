493d5528a1acb3c62a3623053b0c1787e5b8d26c
Alexander Radchenko <radchenkosasha@gmail.com>
2022-12-09 04:51:18 +0600

    Add Competition codewars.com

---
+++ src/Competitions/codewars.com/Permutations/Permutations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Permutations
+{
+    // https://www.codewars.com/kata/5254ca2719453dcc0b00027d
+    public static List<string> SinglePermutations(string s) =>
+        GetPermutations(s).Select(p => string.Join("", p)).ToList();
+
+    public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> items)
+    {
+        var count = items.Count();
+        if (count == 0)
+        {
+            yield return Array.Empty<T>();
+            yield break;
+        }
+
+        var used = new HashSet<T>();
+        foreach (var item in items.Select((it, i) => (it, i)))
+        {
+            if (used.Contains(item.Item1))
+                continue;
+
+            foreach (var p in GetPermutations(items.Take(item.Item2).Concat(items.Skip(item.Item2 + 1))))
+                yield return Enumerable.Repeat(item.Item1, 1).Concat(p);
+
+            used.Add(item.Item1);
+        }
+    }
+}
---
+++ src/Competitions/codewars.com/Range Extraction/RangeExtraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+public class RangeExtraction
+{
+    public static string Extract(int[] args)
+    {
+        var builder = new StringBuilder();
+        for (var i = 0; i < args.Length; i++)
+        {
+            var j = i;
+            while (i + 1 < args.Length && args[i] + 1 == args[i + 1])
+                ++i;
+            j = i - j;
+            builder.Append(j switch
+            {
+                0 => args[i].ToString(),
+                1 => $"{args[i] - j},{args[i]}",
+                _ => $"{args[i] - j}-{args[i]}",
+            });
+            builder.Append(',');
+        }
+        if (builder.Length > 0)
+            builder.Remove(builder.Length - 1, 1);
+
+        return builder.ToString();
+    }
+}
