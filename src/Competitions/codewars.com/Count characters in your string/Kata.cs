e0eb1beead2d45360b236bc6c68da86dd8bba3bd
Alexander Radchenko <radchenkosasha@gmail.com>
2022-12-09 07:34:30 +0600

    Add // Count characters in your string // https://www.codewars.com/kata/52efefcbcdf57161d4000091

---
+++ src/Competitions/codewars.com/Count characters in your string/Kata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Count characters in your string
+// https://www.codewars.com/kata/52efefcbcdf57161d4000091
+public class Kata
+{
+    public static Dictionary<char, int> Count(string str) =>
+      str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+}
