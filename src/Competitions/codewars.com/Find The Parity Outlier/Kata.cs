4bc701852457015a3f9f266e7e5c706162a28563
Alexander Radchenko <radchenkosasha@gmail.com>
2022-12-09 07:55:45 +0600

    Add // Find The Parity Outlier // https://www.codewars.com/kata/5526fc09a1bbd946250002dc

---
+++ src/Competitions/codewars.com/Find The Parity Outlier/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+// Find The Parity Outlier
+// https://www.codewars.com/kata/5526fc09a1bbd946250002dc
+public class Kata
+{
+    public static int Find(int[] integers)
+    {
+        if (integers.Take(3).Where(x => (x & 1) == 0).Count() > 1)
+            return integers.Where(x => (x & 1) == 1).First();
+        return integers.Where(x => (x & 1) == 0).First();
+    }
+}
