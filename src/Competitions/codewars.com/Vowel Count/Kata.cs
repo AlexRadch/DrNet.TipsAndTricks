3d36fcac1309ca24a1b185c56ffbc4ac48f77cee
Alexander Radchenko <radchenkosasha@gmail.com>
2022-12-09 07:58:12 +0600

    Add // Vowel Count // https://www.codewars.com/kata/54ff3102c1bad923760001f3

---
+++ src/Competitions/codewars.com/Vowel Count/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+// Vowel Count
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+public static class Kata
+{
+    static char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+    public static int GetVowelCount(string str)
+    {
+        int start = 0;
+        int vowelCount = 0;
+        while ((start = str.IndexOfAny(vowels, start) + 1) > 0)
+            ++vowelCount;
+
+        return vowelCount;
+    }
+}
