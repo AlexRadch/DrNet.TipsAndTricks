40b11be352120d5958feab8c25714237fa07d055
Alexander Radchenko <radchenkosasha@gmail.com>
2022-12-09 07:19:00 +0600

    Add // Replace With Alphabet Position // https://www.codewars.com/kata/546f922b54af40e1e90001da

---
+++ src/Competitions/codewars.com/Replace With Alphabet Position/Kata.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+// Replace With Alphabet Position
+// https://www.codewars.com/kata/546f922b54af40e1e90001da
+public static class Kata
+{
+    public static string AlphabetPosition(string text) =>
+      string.Join(" ", text.ToLower().Where(char.IsLetter).Select(x => x - 'a' + 1));
+}
