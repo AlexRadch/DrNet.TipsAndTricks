a2e82eb59d1593eb8fdc2e30b15788908491cfec
Alexander Radchenko <radchenkosasha@gmail.com>
2023-01-26 02:16:14 +0600

    // Thue-Morse Sequence // https://www.codewars.com/kata/591aa1752afcb02fa300002a

---
+++ src/Competitions/codewars.com/Thue-Morse Sequence/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Text;
+
+// Thue-Morse Sequence
+// https://www.codewars.com/kata/591aa1752afcb02fa300002a
+public class Kata
+{
+    // https://en.wikipedia.org/wiki/Thue%E2%80%93Morse_sequence
+    public static string ThueMorse(int n)
+    {
+        var result = new StringBuilder(n);
+
+        var bit = 0;
+        for (uint i = 0; i < n; i++)
+        {
+            var x = BitOperations.Log2(i ^ (i - 1));
+            if ((x & 1) == 0)
+                bit = 1 - bit;
+            result.Append(bit);
+        }
+
+        return result.ToString();
+    }
+}
