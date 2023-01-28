5c7a4b0974a9ac6a4de926b56797fdbc07d4a3a2
Alexander Radchenko <radchenkosasha@gmail.com>
2023-01-28 15:33:59 +0600

    // BECOME IMMORTAL // https://www.codewars.com/kata/59568be9cc15b57637000054

---
+++ src/Competitions/codewars.com/BECOME IMMORTAL/Immortal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// BECOME IMMORTAL
+// https://www.codewars.com/kata/59568be9cc15b57637000054
+public static class Immortal
+{
+    /// set true to enable debug
+    public static bool Debug = false;
+
+    public static long ElderAge(long n, long m, long k, long newp)
+    {
+        if (n != m)
+        {
+            var max = Math.Max(n, m);
+            var min = Math.Min(n, m);
+            return (ElderAge(max, max, k, newp) + ElderAge(min, min, k, newp)) / 2 % newp;
+        }
+        var nk = n - k;
+        if (nk <= 0)
+            return 0;
+        var result = nk * (nk - 1) / 2 * n;
+        return result;
+    }
+}
