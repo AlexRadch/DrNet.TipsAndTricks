8f43e2a40999290ca05ddcc5d6d119a51bf5e392
Alexander Radchenko <radchenkosasha@gmail.com>
2024-12-04 15:19:20 +0700

    AdventOfCode 2024 Day3 Part1

---
+++ src/Competitions/adventofcode.com/2024/Day3.Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.RegularExpressions;
+
+{
+    var input = File.OpenText("input1.txt").ReadToEnd();
+
+    var result = Solve(input);
+
+    Console.WriteLine($"{result}");
+}
+
+{
+    var input = File.OpenText("input2.txt").ReadToEnd();
+
+    var result = Solve(input);
+
+    Console.WriteLine($"{result}");
+}
+
+static long Solve(string input) =>
+    MyRegex().Matches(input)
+        .Select(match => long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value))
+        .Sum();
+
+partial class Program
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private static partial Regex MyRegex();
+}
