b73cc64a0f38f32b7a9915e3587a532c4afc96f1
Alexander Radchenko <radchenkosasha@gmail.com>
2024-12-10 12:24:57 +0700

    rename

---
+++ src/Competitions/adventofcode.com/2024/Day10.Part1/Program.cs
@@ -0,0 +1,21 @@
+﻿//  --- Day 10: Hoof It ---
+//  https://adventofcode.com/2024/day/10
+
+{
+    var map = File.ReadLines("input1.txt").Where(line => line?.Length > 0).ToArray();
+
+    int result = Solve(map);
+    Console.WriteLine($"{result}");
+}
+
+{
+    var map = File.ReadLines("input1.txt").Where(line => line?.Length > 0).ToArray();
+
+    int result = Solve(map);
+    Console.WriteLine($"{result}");
+}
+
+static int Solve<TMap>(TMap map) where TMap : IReadOnlyList<string>
+{
+    return 0;
+}
