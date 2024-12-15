cfcf76c577734c8772030c4b4edd1a1047d81d9d
Alexander Radchenko <radchenkosasha@gmail.com>
2024-12-15 16:28:34 +0700

    AdventOfCode 2024 Day14 Part2

--- src/Competitions/adventofcode.com/2024/Day14.Part1/Program.cs
+++ src/Competitions/adventofcode.com/2024/Day14.Part1/Program.cs
@@ -4,56 +4,57 @@
 using System.Text.RegularExpressions;
 
 using Robot = (int X, int Y, int DX, int DY);
+using Point = (int X, int Y);
 
 {
     var robots = File.ReadLines("input1.txt")
-        .Select(ReadRobot)
-        .ToArray().AsReadOnly();
+    .Select(ReadRobot)
+    .ToArray().AsReadOnly();
 
-    var result = Solve(robots, 11, 7);
+var result = Solve(robots, 11, 7, 100);
 
-    Console.WriteLine($"{result}");
+Console.WriteLine($"{result}");
 }
 
 {
-    var robots = File.ReadLines("input2.txt")
-        .Select(ReadRobot)
-        .ToArray().AsReadOnly();
+var robots = File.ReadLines("input2.txt")
+    .Select(ReadRobot)
+    .ToArray().AsReadOnly();
 
-    var result = Solve(robots, 101, 103);
+var result = Solve(robots, 101, 103, 100);
 
-    Console.WriteLine($"{result}");
+Console.WriteLine($"{result}");
 }
 
 static Robot ReadRobot(string line)
 {
-    var matches = SignIntRegex().Matches(line);
+var matches = SignIntRegex().Matches(line);
 
-    return new Robot(
-        int.Parse(matches[0].Value), int.Parse(matches[1].Value),
-        int.Parse(matches[2].Value), int.Parse(matches[3].Value));
+return new Robot(
+    int.Parse(matches[0].Value), int.Parse(matches[1].Value),
+    int.Parse(matches[2].Value), int.Parse(matches[3].Value));
 }
 
-static int Solve<TRobots>(TRobots robots, int wide, int tall) where TRobots: IEnumerable<Robot>
+static int Solve<TRobots>(TRobots robots, int wide, int tall, int seconds) where TRobots: IEnumerable<Robot>
 {
-    var positions = robots.Select(robot => Evaluate(robot, wide, tall, 100));
-    var quadrants = positions.GroupBy(position => Quadrant(position.X, position.Y, wide, tall)); //.ToArray();
-    quadrants = quadrants.Where(group => group.Key > 0); //.ToArray();
+    var points = robots.Select(robot => Evaluate(robot, wide, tall, seconds));
+    var quadrants = points.GroupBy(position => Quadrant(position.X, position.Y, wide, tall)); //.ToArray();
+    quadrants = quadrants.Where(group => group.Key >= 0); //.ToArray();
     var counts = quadrants.Select(group => group.Count());
     var result = counts.Aggregate(1, (result, count) => result * count);
     return result;
 }
 
-static (int X, int Y) Evaluate(Robot robot, int wide, int tall, int steps) =>
-    (((robot.X + robot.DX * steps) % wide + wide) % wide,
-    ((robot.Y + robot.DY * steps) % tall + tall) % tall);
+static Point Evaluate(Robot robot, int wide, int tall, int seconds) =>
+    checked(
+        (((robot.X + robot.DX * seconds) % wide + wide) % wide,
+        ((robot.Y + robot.DY * seconds) % tall + tall) % tall)
+    );
 
 static int Quadrant(int X, int Y, int wide, int tall) =>
     X == (wide / 2) || Y == (tall / 2)
-    ? 0
-    : 1 + X / (wide / 2 + 1) + Y / (tall / 2 + 1) * 2;
-
-
+    ? -1
+    : X / (wide / 2 + 1) + Y / (tall / 2 + 1) * 2;
 
 partial class Program
 {
---
+++ src/Competitions/adventofcode.com/2024/Day14.Part2/Program.cs
@@ -0,0 +1,162 @@
+﻿//  --- Day 14: Restroom Redoubt ---
+//  https://adventofcode.com/2024/day/14
+
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+using Robot = (int X, int Y, int DX, int DY);
+using Point = (int X, int Y);
+using System.Text;
+
+{
+    var robots = File.ReadLines("input1.txt")
+        .Select(ReadRobot)
+        .ToArray().AsReadOnly();
+
+    var result = Solve(robots, 11, 7);
+
+    Console.WriteLine($"{result}");
+}
+
+{
+    var robots = File.ReadLines("input2.txt")
+        .Select(ReadRobot)
+        .ToArray().AsReadOnly();
+
+    var result = Solve(robots, 101, 103);
+
+    Console.WriteLine($"{result}");
+}
+
+static Robot ReadRobot(string line)
+{
+    var matches = SignIntRegex().Matches(line);
+
+    return new Robot(
+        int.Parse(matches[0].Value), int.Parse(matches[1].Value),
+        int.Parse(matches[2].Value), int.Parse(matches[3].Value));
+}
+
+static int Solve<TRobots>(TRobots robots, int wide, int tall) where TRobots : IEnumerable<Robot>
+{
+    var totalCount = robots.Count();
+    var wideHalf = wide / 2;
+
+    IEnumerable<Point> max = [];
+    int result = -1;
+    for (var seconds = 0; seconds < 10000; seconds++)
+    {
+       
+        var points = robots.Select(robot => Evaluate(robot, wide, tall, seconds));
+        var picture = MaxPicture(points);
+
+        if (picture.Count() > max.Count())
+        {
+            max = picture;
+            result = seconds;
+
+            var map = MapString(picture);
+            Console.WriteLine(map);
+
+            if (picture.Count() * 2 > totalCount)
+                return result;
+        }
+    }
+    return result;
+}
+
+static Point Evaluate(Robot robot, int wide, int tall, int seconds) =>
+    checked(
+        (((robot.X + robot.DX * seconds) % wide + wide) % wide,
+        ((robot.Y + robot.DY * seconds) % tall + tall) % tall)
+    );
+
+static IEnumerable<Point> MaxPicture(IEnumerable<Point> points)
+{
+    var set = points.ToHashSet();
+    var visited = new HashSet<Point>();
+
+    IEnumerable<Point> result = [];
+    foreach (var point in set)
+    {
+        if (visited.Contains(point))
+            continue;
+
+        var picture = GetPicture(set, point);
+        visited.UnionWith(picture);
+
+        if (picture.Count() > result.Count())
+            result = picture;
+    }
+
+    //{
+    //    var map = MapString(set);
+    //    Console.WriteLine(map);
+    //}
+    //{
+    //    var map = MapString(result);
+    //    Console.WriteLine(map);
+    //}
+
+    return result;
+}
+
+static IEnumerable<Point> GetPicture(ISet<Point> points, Point start)
+{
+    var result = new HashSet<Point>();
+
+    var queue = new Queue<Point>();
+    queue.Enqueue(start);
+
+    while (queue.Count > 0)
+    {
+        var point = queue.Dequeue();
+        if (!result.Add(point))
+            continue;
+
+        foreach(var (X, Y) in Directions)
+        {
+            var next = new Point(point.X + X, point.Y + Y);
+            if (result.Contains(next) || !points.Contains(next))
+                continue;
+            queue.Enqueue(next);
+        }
+    }
+
+    return result;
+}
+
+static string MapString(IEnumerable<Point> points)
+{
+    var width = points.Select(point => point.X).Max() + 1;
+    var height = points.Select(point => point.Y).Max() + 1;
+    var result = new StringBuilder(width * height);
+
+    for (var y = 0; y < height; y++)
+    {
+        for (var x = 0; x < width; x++)
+        {
+            if (points.Contains((x, y)))
+                result.Append('#');
+            else
+                result.Append('.');
+        }
+        result.AppendLine();
+    }
+
+    return result.ToString();
+}
+
+partial class Program
+{
+    [GeneratedRegex(@"-?\d+")]
+    private static partial Regex SignIntRegex();
+
+    static readonly ReadOnlyCollection<Point> Directions = new Point[]
+        {
+            (-1, -1), (0, -1), (1, -1),
+            (1, 0), (1, 1),
+            (0, 1), (-1, 1),
+            (-1, 0),
+        }.AsReadOnly();
+}
