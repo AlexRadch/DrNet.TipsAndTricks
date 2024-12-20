5829f1edfe0476624f32fff58b672499d965933c
Alexander Radchenko <radchenkosasha@gmail.com>
2024-12-21 01:23:16 +0700

    Enhance code clarity and functionality with documentation
    - Added XML documentation comments to several methods for better understanding.

--- src/Competitions/adventofcode.com/2024/Day20.Part2/Program.cs
+++ src/Competitions/adventofcode.com/2024/Day20.Part2/Program.cs
@@ -7,6 +7,12 @@ using Cheat = ((int X, int Y) P1, (int X, int Y) P2, int Save);
 ProcessFile("input1.txt", 20, 50);
 ProcessFile("input2.txt", 20, 100);
 
+/// <summary>
+/// Processes the input file to find valid cheats and prints the count.
+/// </summary>
+/// <param name="filePath">The path to the input file.</param>
+/// <param name="maxDuration">The maximum duration for a cheat.</param>
+/// <param name="minSave">The minimum save value for a cheat to be considered valid.</param>
 static void ProcessFile(string filePath, int maxDuration, int minSave)
 {
     using var reader = File.OpenText(filePath);
@@ -22,9 +28,22 @@ static void ProcessFile(string filePath, int maxDuration, int minSave)
     Console.WriteLine(result);
 }
 
+/// <summary>
+/// Reads the map from the provided TextReader.
+/// </summary>
+/// <param name="reader">The TextReader to read the map from.</param>
+/// <returns>An enumerable collection of strings representing the map.</returns>
 static IEnumerable<string> ReadMap(TextReader reader) =>
     reader.ReadLines();
 
+/// <summary>
+/// Solves the map to find valid cheats.
+/// </summary>
+/// <typeparam name="TMap">The type of the map.</typeparam>
+/// <param name="map">The map to solve.</param>
+/// <param name="maxDuration">The maximum duration for a cheat.</param>
+/// <param name="minSave">The minimum save value for a cheat to be considered valid.</param>
+/// <returns>An enumerable collection of valid cheats.</returns>
 static IEnumerable<Cheat> Solve<TMap>(TMap map, int maxDuration, int minSave) where TMap : IEnumerable<string>
 {
     var height = map.Count();
@@ -39,10 +58,27 @@ static IEnumerable<Cheat> Solve<TMap>(TMap map, int maxDuration, int minSave) wh
     return PaveCheats(costMap, maxDuration, minSave);
 }
 
+/// <summary>
+/// Finds valid cheats in the map.
+/// </summary>
+/// <typeparam name="TMap">The type of the map.</typeparam>
+/// <param name="map">The map to find cheats in.</param>
+/// <param name="maxDuration">The maximum duration for a cheat.</param>
+/// <param name="minSave">The minimum save value for a cheat to be considered valid.</param>
+/// <returns>An enumerable collection of valid cheats.</returns>
 static IEnumerable<Cheat> PaveCheats<TMap>(TMap map, int maxDuration, int minSave) where TMap : IList<IList<int>> =>
     map.SelectMany((row, y) => row.SelectMany((value, x) => value >= 0 ? PointCheats(map, (x, y), maxDuration, minSave) : []));
 
 
+/// <summary>
+/// Calculates possible cheats from a given point.
+/// </summary>
+/// <typeparam name="TMap">The type of the map.</typeparam>
+/// <param name="map">The map to calculate cheats in.</param>
+/// <param name="p">The point to calculate cheats from.</param>
+/// <param name="maxDuration">The maximum duration for a cheat.</param>
+/// <param name="minSave">The minimum save value for a cheat to be considered valid.</param>
+/// <returns>An enumerable collection of valid cheats from the given point.</returns>
 static IEnumerable<Cheat> PointCheats<TMap>(TMap map, Point p, int maxDuration, int minSave)
     where TMap : IList<IList<int>>
 {
@@ -69,6 +105,13 @@ static IEnumerable<Cheat> PointCheats<TMap>(TMap map, Point p, int maxDuration, 
     }
 }
 
+/// <summary>
+/// Paves a track from the start to the end point, marking the cost of each step.
+/// </summary>
+/// <typeparam name="TMap">The type of the map.</typeparam>
+/// <param name="map">The map to pave the track in.</param>
+/// <param name="start">The start point of the track.</param>
+/// <param name="end">The end point of the track.</param>
 static void PaveTrack<TMap>(TMap map, Point start, Point end) where TMap : IList<IList<int>>
 {
     var cost = 0;
@@ -102,9 +145,20 @@ static void PaveTrack<TMap>(TMap map, Point start, Point end) where TMap : IList
 
 static class Extensions
 {
+    /// <summary>
+    /// Flattens a 2D map into an enumerable collection of positions and values.
+    /// </summary>
+    /// <typeparam name="T">The type of the values in the map.</typeparam>
+    /// <param name="map">The 2D map to flatten.</param>
+    /// <returns>An enumerable collection of positions and values.</returns>
     public static IEnumerable<(Point Position, T Value)> FlatMap<T>(this IEnumerable<IEnumerable<T>> map) =>
         map.SelectMany((row, y) => row.Select((value, x) => ((x, y), value)));
 
+    /// <summary>
+    /// Reads lines from a TextReader.
+    /// </summary>
+    /// <param name="reader">The TextReader to read lines from.</param>
+    /// <returns>An enumerable collection of lines.</returns>
     public static IEnumerable<string> ReadLines(this TextReader reader)
     {
         while (reader.ReadLine() is string line)
